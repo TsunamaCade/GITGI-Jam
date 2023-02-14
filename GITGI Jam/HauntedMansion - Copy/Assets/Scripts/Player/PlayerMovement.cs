ad0bca0808ddc52fba942a21c508a08703f11427
Connerallen75 <117148008+Connerallen75@users.noreply.github.com>
2023-02-13 22:00:07 -0700

    Unity project level build for haunted mansion

---
+++ GITGI Jam/HauntedMansion - Copy/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void FixedUpdate()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        rb.velocity = moveInput * moveSpeed;
+    }
+}
